3d687853a0d2277761f01ae3ff796c6a17b0ab9e
super-potoooooooo <67077815+super-potoooooooo@users.noreply.github.com>
2020-06-17 17:59:57 -0400

    Add files via upload

---
+++ calculators.py
@@ -0,0 +1,22 @@
+#thanks for purchasing meh calculator
+
+
+a = float(input("Input a: ")) #gives valyoo for a
+b = float(input("Input b: "))
+c = float(input("er put ur c stuffs i guess :"))
+#change the equiation here ↓
+x = (c - b) / a
+
+print("teh solooshin to ur equashin iz", x)
+
+#Tests to see if it working
+#x + 0 = 5 expect x = 5
+#x + 5 = 9 x should be 4
+
+
+#SQUARE ROOOT CALCULATOOOOOOOOOOOOOOOOOOOOOOOOOOOR
+usergivestuffs = float(input("put ur numbah heres i guess: "))
+print(usergivestuffs ** (1/2))
+
+
+#if you only want to use 1 calcuator put a hastag before the lines of code
